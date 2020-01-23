dd800d7bafb7c15ce96cc99c5e12562027c13d17
Miriam Aguirre Account <mimi_miriam@byui.edu>
2020-01-23 09:30:37 -0600

    flex menu

--- lesson3/js/todaydate.js
+++ lesson3/js/todaydate.js
@@ -1,7 +1,7 @@
 
 var today = new Date();
-var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
-var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-var dateTime = date+' '+time;
-
+var date = (today.getMonth()+1) + "/" + today.getDate() + "/" + today.getFullYear();
+/*var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+var dateTime = date+' '+time;*/
+var dateTime = date;
 document.write("Today is: " + dateTime);
