6f6b9c21cf48cc0a2db154aad81ed9261b90bc07
Miriam Aguirre Account <mimi_miriam@byui.edu>
2020-04-03 15:22:43 -0500

    todaydate

--- canyonfishadventures/js/todaydate.js
+++ canyonfishadventures/js/todaydate.js
@@ -1,11 +1,5 @@
-//var today = new Date();
-//var formatOptions = {weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
-//document.write(today.toLocaleDateString('en-US', formatOptions));
-//or
-//document.getElelementById("curentdate").textContent = currentdate;
 
-
-let m = new Date();
+/*let m = new Date();
 
 var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 var weekday = days[m.getDay()];
@@ -19,3 +13,13 @@ let year = m.getFullYear();
 
 let dateString = weekday + ", " + month + " " + day + ", " + year + " ";
 document.getElementById("todayDate").textContent = dateString;
+*/
+
+//both methods work
+
+var today = new Date();
+var formatOptions = {weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+var currentdate = today.toLocaleDateString('en-US', formatOptions);
+
+
+document.getElementById("todaydate").textContent = currentdate;
